43db98b3b983192e31c88eea619acbe88f2252cb
Samppa <samppasalo16@gmail.com>
2022-11-25 16:34:02 +0200

    create code and documentation for ConsoleIO

--- src/console_io.py
+++ src/console_io.py
@@ -1,1 +1,34 @@
-# To be implemented
+class ConsoleIO:
+    """Module for user input."""
+
+    def get_command(self):
+        """Asks command from the user
+
+        Returns:
+            str: lower case command as str
+        """
+        return input("Input command: ").lower()
+
+    def get_reference(self):
+        """Asks reference from the user
+
+        Returns:
+            list: authors, title, year published and publisher as list
+        """
+
+        authors = ""
+        while True:
+            author = input("Input author in format lastname, firstname or input q to stop: ")
+            if author == "q":
+                if len(authors) > 0:
+                    authors = authors[0:-5]
+                    break
+                else:
+                    print("At least one author is required")
+                    continue
+            else:
+                authors += author + " and "
+        title = input("Input title: ")
+        year = input("Input year published: ")
+        publisher = input("Input publisher: ")
+        return [authors, title, year, publisher]
