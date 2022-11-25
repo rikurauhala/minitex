287350ad94a9082d5938fc8b61a3324f8c5c90b5
Henri Remonen <henri@remonen.fi>
2022-11-25 08:54:18 +0200

    Create key generator for the reference class

--- src/reference.py
+++ src/reference.py
@@ -1,12 +1,12 @@
+import re
+
 class Reference:
 	def __init__(self, author: str, title: str, year: str, publisher: str) -> None:
 		self._author = author
 		self._title = title
 		self._year = year
 		self._publisher = publisher
-
 		self._key = self._gen_key()
-
 
 	@property
 	def author(self):
@@ -41,20 +41,45 @@ class Reference:
 		}
 		return reference
 
-	def _gen_key(self):
-		return
+	def _gen_key(self) -> str:
+		"""Generates key attribute for reference based on the authors lastnames
+		and the publishing year
+
+		Returns:
+			str: generated key for the reference object
+		"""
+		authors = re.split(' and ', self._author)
+		names_splitted = [lname.split(" ") for lname in authors]
+		last_names = []
+
+		for name in names_splitted:
+			try:
+				if "," in name[0]:
+					last_names.append(name[0].strip(","))
+				else:
+					last_names.append(name[-1])
+			except IndexError:
+				last_names.append(name[0].strip(","))
+
+		if len(last_names) == 1:
+			key = last_names[0]
+		else:
+			key = ""
+
+			for name in last_names:
+				key += name[0]
+
+		key += self.year[-2:]
+
+		return key
 
 
 if __name__ == "__main__":
-	author = "Allan Collins and John Seely Brown and Ann Holum"
+	author = "Martin, Robert"
 	title = "Cognitive apprenticeship: making thinking visible"
-	journal = "American Educator"
+	publisher = "Prentice Hall"
 	year = "1991"
-	r = Reference(author, title, journal, year)
-	
-	print(r.author)
-	print(r.title)
-	print(r.publisher)
-	print(r.year)
+	r = Reference(author, title, year, publisher)
 
 	print(r.data)
+	
