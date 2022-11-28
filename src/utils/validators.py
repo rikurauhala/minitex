e193be81ff6370e46d68f1e37578acd22004c28f
Henri Remonen <henri@remonen.fi>
2022-11-28 12:18:34 +0200

    Create validator class to validate class attributes

---
+++ src/utils/validators.py
@@ -0,0 +1,18 @@
+def validate_attribute_string(to_validate: str, attribute: str) -> bool:
+    """Validator for string attributes of classes
+
+    Args:
+        to_validate (str): value to validate
+        type (str): attribute name for error message
+
+    Returns:
+        bool: True if validation was success
+    """
+
+    if not isinstance(to_validate, str):
+        raise TypeError(f"Expected {attribute} of value str, instead got: ",
+            type(to_validate))
+    if not to_validate:
+        raise ValueError(f"{attribute} cannot be empty")
+
+    return True
