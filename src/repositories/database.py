98245021f51a99df04d2fac35243716dcec80175
Samppa <samppasalo16@gmail.com>
2022-12-02 18:22:36 +0200

    edit method names in database.py

--- src/repositories/database.py
+++ src/repositories/database.py
@@ -9,9 +9,9 @@ class Repository:
     def create_table(self):
         self.cursor.execute("CREATE TABLE references (id INTEGER PRIMARY KEY, author TEXT, title TEXT, year INTEGER, publisher TEXT);")
 
-    def add_reference(self, authors, title, year, publisher):
+    def add(self, authors, title, year, publisher):
         self.cursor.execute("INSERT INTO references (author, title, year, publisher) VALUES (?, ?, ?, ?)", (authors, title, int(year), publisher))
 
-    def get_references(self):
+    def find_all(self):
         references = self.cursor.execute("SELECT * FROM references")
         return references
