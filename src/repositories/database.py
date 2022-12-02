cad0c3535ed1e31b63e3d65c17635b2f2063cf0a
Samppa <samppasalo16@gmail.com>
2022-12-02 18:18:22 +0200

    add database.py

---
+++ src/repositories/database.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+class Repository:
+    
+    def __init__(self):
+        self.connection = sqlite3.connect("references.db")
+        self.cursor = self.connection.cursor()
+
+    def create_table(self):
+        self.cursor.execute("CREATE TABLE references (id INTEGER PRIMARY KEY, author TEXT, title TEXT, year INTEGER, publisher TEXT);")
+
+    def add_reference(self, authors, title, year, publisher):
+        self.cursor.execute("INSERT INTO references (author, title, year, publisher) VALUES (?, ?, ?, ?)", (authors, title, int(year), publisher))
+
+    def get_references(self):
+        references = self.cursor.execute("SELECT * FROM references")
+        return references
