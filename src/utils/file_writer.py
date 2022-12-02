b22477e5fa01c8228eef1f777f58f5ca38ce9d78
Henri Remonen <henri@remonen.fi>
2022-12-02 13:39:04 +0200

    Create method for folder selection

--- src/utils/file_writer.py
+++ src/utils/file_writer.py
@@ -2,6 +2,14 @@ import tkinter as tk
 from tkinter import filedialog
 
 class FileWriter:
+    """Class for writing Bibtex files
+    """
     def __init__(self) -> None:
+        """Create filewriter instance
+        """
         self._root = tk.Tk()
         self._root.withdraw()
+
+    def _get_user_directory(self):
+        directory = filedialog.askdirectory(title="Select Folder")
+        return directory
