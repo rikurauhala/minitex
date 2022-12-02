58052fbe4d20c61c9ff191f4eb3c72bfe38c3f76
Samppa <samppasalo16@gmail.com>
2022-12-02 19:19:43 +0200

    edit database.py to save after inserting

--- src/repositories/database.py
+++ src/repositories/database.py
@@ -5,7 +5,10 @@ class Repository:
     def __init__(self):
         self.connection = sqlite3.connect("references.db")
         self.cursor = self.connection.cursor()
-        self.create_table()
+        try:
+            self.create_table()
+        except:
+            pass
 
     def create_table(self):
         self.cursor.execute("CREATE TABLE bookreferences (id INTEGER PRIMARY KEY, author TEXT, title TEXT, year INTEGER, publisher TEXT);")
@@ -13,7 +16,8 @@ class Repository:
     def add(self, reference):
         authors, title, year, publisher = reference.author, reference.title, reference.year, reference.publisher
         self.cursor.execute("INSERT INTO bookreferences (author, title, year, publisher) VALUES (?, ?, ?, ?)", (authors, title, int(year), publisher))
-
+        self.connection.commit()
+        
     def find_all(self):
         references = self.cursor.execute("SELECT * FROM bookreferences")
         return references
