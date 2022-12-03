ebac2ceb6ea65727b93274aeea4e1f05f69505b0
Riku Rauhala <riku@rauhala.io>
2022-12-03 13:08:20 +0200

    Create function drop_tables

--- src/repositories/database.py
+++ src/repositories/database.py
@@ -1,23 +1,43 @@
 import sqlite3
 
+
 class Repository:
-    
     def __init__(self):
         self.connection = sqlite3.connect("references.db")
         self.cursor = self.connection.cursor()
         try:
             self.create_table()
-        except:
+        except sqlite3.OperationalError:
             pass
 
     def create_table(self):
-        self.cursor.execute("CREATE TABLE bookreferences (id INTEGER PRIMARY KEY, author TEXT, title TEXT, year INTEGER, publisher TEXT);")
+        self.cursor.execute("""
+            CREATE TABLE bookreferences (
+                id INTEGER PRIMARY KEY,
+                author TEXT,
+                title TEXT,
+                year INTEGER,
+                publisher TEXT
+            )"""
+        )
+    
+    def drop_tables(self):
+        self.cursor.execute("""
+            DROP TABLE IF EXISTS bookreferences;
+        """)
+        self.connection.commit()
 
     def add(self, reference):
-        authors, title, year, publisher = reference.author, reference.title, reference.year, reference.publisher
-        self.cursor.execute("INSERT INTO bookreferences (author, title, year, publisher) VALUES (?, ?, ?, ?)", (authors, title, int(year), publisher))
+        authors = reference.author
+        title = reference.title
+        year = reference.year
+        publisher = reference.publisher
+        self.cursor.execute(
+            "INSERT INTO bookreferences (author, title, year, publisher) VALUES (?, ?, ?, ?)",
+            (authors, title, int(year), publisher)
+        )
         self.connection.commit()
-        
+
     def find_all(self):
         references = self.cursor.execute("SELECT * FROM bookreferences")
         return references
