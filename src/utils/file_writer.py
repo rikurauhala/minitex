d1479b3143fe6ae8458b6f7c8509e29566e8f181
Henri Remonen <henri@remonen.fi>
2022-12-05 08:14:18 +0200

    Create method for entry construction

--- src/utils/file_writer.py
+++ src/utils/file_writer.py
@@ -11,7 +11,11 @@ class FileWriter:
         self._root = tk.Tk()
         self._root.withdraw()
         self._file_name = "references.bib"
-        self._folder_path = Path(folder_path)
+
+        if folder_path:
+            self._folder_path = Path(folder_path)
+        else:
+            self._folder_path = None
 
     def _get_user_directory(self) -> Path:
         """Prompt user for directory where to save the Bibtex file
@@ -21,6 +25,24 @@ class FileWriter:
         """
         directory = filedialog.askdirectory(title="Select Folder")
         return Path(directory)
+
+    def _make_entry_string(self, ref: dict) -> str:
+        """Create entry string from reference to be saved in Bibtex file
+
+        Return:
+            str: entry to save in the file
+        """
+        if 'type' not in ref:
+            ref_type = "@BOOK"
+        else: ref_type = ref['type']
+
+        return f"""{ref_type}{"{"}{ref['key']},
+    title = "{ref['title']}",
+    author = "{ref['authors']}",
+    publisher = "{ref['publisher']}",
+    year = "{ref['year']}"
+{"}"}
+"""
 
     def write_bibtex(self, references: list) -> None:
         """Write given list of reference objects to a Bibtex file
@@ -32,23 +54,14 @@ class FileWriter:
 
         with open(file_path, "a", encoding="utf-8") as file:
             for ref in references:
-                if 'type' not in ref:
-                    ref_type = "@BOOK"
-                else: ref_type = ref['type']
-
-                entry = f"""{ref_type}{"{"}{ref['key']},
-    title = "{ref['title']}",
-    author = "{ref['authors']}",
-    publisher = "{ref['publisher']}",
-    year = "{ref['year']}"
-{"}"}
-    """
+                entry = self._make_entry_string(ref)
+                
                 file.write("\n")
                 file.write(entry)
 
-
 if __name__ == "__main__":
-    f = FileWriter(folder_path="./src/tests/")
+    #f = FileWriter(folder_path="./src/tests/")
+    f = FileWriter()
     test = [{
             'key': 'Martin91',
             'authors': 'Martin, Robert',
