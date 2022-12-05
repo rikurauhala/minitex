55a2afcc3cc6a15746222ab9457c8a19211bc2d4
Henri Remonen <henri@remonen.fi>
2022-12-05 09:42:11 +0200

    Create tests for filewriter

--- src/tests/utils/test_file_writer.py
+++ src/tests/utils/test_file_writer.py
@@ -1,9 +1,12 @@
+import os.path
 import unittest
+import tempfile
+
 from utils.file_writer import FileWriter
 
 class TestFileWriter(unittest.TestCase):
     def setUp(self) -> None:
-        self.test_writer = FileWriter(folder_path="./src/tests")
+        self.folder_path = "./src/tests"
         self.test_entries = [{
                 'key': 'Martin91',
                 'authors': 'Martin, Robert',
@@ -26,3 +29,18 @@ class TestFileWriter(unittest.TestCase):
         with self.assertRaises(TypeError):
             f = FileWriter(folder_path="123")
             f.write_bibtex()
+
+    def testWriteBibtexWritesToFile(self):
+        with tempfile.TemporaryDirectory(dir=self.folder_path) as tmp_dir:
+            self.test_writer = FileWriter(tmp_dir)
+            self.test_writer.write_bibtex(self.test_entries)
+
+            self.assertTrue(os.path.isdir(tmp_dir))
+
+            with open(tmp_dir + "/references.bib", "r") as references:
+                content = references.read(6)
+                expected_content = "\n@BOOK"
+                self.assertEqual(content, expected_content)
+                
+
+
