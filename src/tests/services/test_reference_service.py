6d9c06474755c5b1251465dfcc3a712cce6c8676
Riku Rauhala <riku@rauhala.io>
2022-12-11 19:29:11 +0200

    Write tests for edit functionality in reference service

--- src/tests/services/test_reference_service.py
+++ src/tests/services/test_reference_service.py
@@ -53,3 +53,39 @@ class TestReferenceRepository(unittest.TestCase):
         self.repository.delete_all()
         references = self.repository.get_references()
         self.assertEqual(len(references), 0)
+
+    def test_edit_reference(self):
+        self.repository.add_reference(self.reference1)
+        stored_ref1 = self.repository.get_references()[0]
+        self.assertEqual(stored_ref1.author, self.reference1["authors"])
+        self.assertEqual(stored_ref1.title, self.reference1["title"])
+        self.assertEqual(stored_ref1.year, self.reference1["year"])
+        self.assertEqual(stored_ref1.publisher, self.reference1["publisher"])
+        new_author = "New author"
+        self.repository.edit_reference(new_author, 1, 1)
+        stored_ref1 = self.repository.get_references()[0]
+        self.assertEqual(stored_ref1.author, new_author)
+        self.assertEqual(stored_ref1.title, self.reference1["title"])
+        self.assertEqual(stored_ref1.year, self.reference1["year"])
+        self.assertEqual(stored_ref1.publisher, self.reference1["publisher"])
+        new_title = "New title"
+        self.repository.edit_reference(new_title, 1, 2)
+        stored_ref1 = self.repository.get_references()[0]
+        self.assertEqual(stored_ref1.author, new_author)
+        self.assertEqual(stored_ref1.title, new_title)
+        self.assertEqual(stored_ref1.year, self.reference1["year"])
+        self.assertEqual(stored_ref1.publisher, self.reference1["publisher"])
+        new_year = "2005"
+        self.repository.edit_reference(new_year, 1, 3)
+        stored_ref1 = self.repository.get_references()[0]
+        self.assertEqual(stored_ref1.author, new_author)
+        self.assertEqual(stored_ref1.title, new_title)
+        self.assertEqual(stored_ref1.year, new_year)
+        self.assertEqual(stored_ref1.publisher, self.reference1["publisher"])
+        new_publisher = "New publisher"
+        self.repository.edit_reference(new_publisher, 1, 4)
+        stored_ref1 = self.repository.get_references()[0]
+        self.assertEqual(stored_ref1.author, new_author)
+        self.assertEqual(stored_ref1.title, new_title)
+        self.assertEqual(stored_ref1.year, new_year)
+        self.assertEqual(stored_ref1.publisher, new_publisher)
