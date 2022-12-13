ef11de5b7e40953e5efb8bf81080adce8f3882d7
isakpulkki <isak@pulkki.me>
2022-12-13 18:33:29 +0200

    Improve DOI tests

--- src/tests/test_console_io.py
+++ src/tests/test_console_io.py
@@ -4,26 +4,41 @@ from console_io import ConsoleIO
 
 
 class StubIO(ConsoleIO):
-    def input(self, message):
-        return "10.1002/9780470209943"
+    def __init__(self):
+        self.first = True
 
+    def input(self, message):
+        if self.first:
+            self.first = False
+            return "10.1002/9780470209943"
+        return "q"
 
 class TestConsoleIO(unittest.TestCase):
     def setUp(self):
         self.IO = StubIO()
         self.reference = self.IO.get_reference_from_doi()
+        self.doi = {
+            "authors": "Abbie Griffin and Stephen Somermeyer",
+            "title": "The PDMA ToolBook 3 for New Product Development",
+            "year": "2007",
+            "publisher": "John Wiley & Sons, Inc."
+        }
+
+    def test_adding_valid_doi_adds_returns_book_reference(self):
+        self.assertEqual(self.doi, self.reference)
 
     def test__doi_returns_the_correct_authors(self):
         self.assertEqual(self.reference.get("authors"),
-                         "Abbie Griffin and Stephen Somermeyer")
+                         self.doi.get("authors"))
 
     def test__doi_returns_the_correct_title(self):
         self.assertEqual(self.reference.get("title"),
-                         "The PDMA ToolBook 3 for New Product Development")
+                         self.doi.get("title"))
 
     def test__doi_returns_the_correct_year(self):
-        self.assertEqual(self.reference.get("year"), "2007")
+        self.assertEqual(self.reference.get("year"), 
+        self.doi.get("year"))
 
     def test__doi_returns_the_correct_publisher(self):
         self.assertEqual(self.reference.get("publisher"),
-                         "John Wiley & Sons, Inc.")
+                         self.doi.get("publisher"))
